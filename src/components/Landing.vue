1404b701ce55a0d9b636b75ddaee5d3227d3a473
alan850627 <alan850627@gmail.com>
2018-03-18 11:05:00 -0500

    customize

---
+++ src/components/Landing.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="landing">
+    <h1>Schedule Us!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Landing',
+
+  components: {
+  },
+
+  props: {
+  },
+
+  // firebase: {
+  // },
+
+  computed: {
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  mounted () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Template.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="template">
+    <h1>Schedule Us!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Template',
+
+  components: {
+  },
+
+  props: {
+  },
+
+  // firebase: {
+  // },
+
+  computed: {
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  mounted () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
