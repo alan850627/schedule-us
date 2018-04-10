9eb942263eda4a20c7cc24b876b3888f6a240508
alan850627 <alan850627@gmail.com>
2018-04-10 00:25:05 -0500

    add component skeleton

---
+++ src/components/calendar/TimeTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="time-table">
+    <h1>Schedule Us!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TimeTable',
+
+  components: {
+  },
+
+  props: {
+  },
+
+  computed: {
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  mounted () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
