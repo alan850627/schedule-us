dcbe095e42dfa295efe3c07450baec3a6f5292ac
alan850627 <alan850627@gmail.com>
2018-04-10 14:05:44 -0500

    add time selector

---
+++ src/components/calendar/TimeSelector.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="time-selector">
+    <h1>Schedule Us!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TimeSelector',
+
+  components: {
+  },
+
+  props: {
+  },
+
+  computed: {
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  mounted () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
